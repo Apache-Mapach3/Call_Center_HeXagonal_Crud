c46e77317cead8b08ea622fcf4f2c697af554e63
Jose <rivera991113@gmail.com>
2025-11-21 21:02:29 -0500

    Estructura completa con clases base

---
+++ CallCenterDesktop/src/main/java/com/udc/callcenterdesktop/Main.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.udc.callcenterdesktop;
+
+
+
+    public class Main {
+    public static void main(String[] args) {
+        // Crear la ventana y mostrarla
+    }
+}
